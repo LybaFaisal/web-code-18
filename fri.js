d229ef6f223d7be84786969435daab6d395401db
Lyba <faisallyba06@gmail.com>
2024-01-05 22:33:15 +0500

    Switch-Statements

---
+++ fri.js
@@ -0,0 +1,32 @@
+//using switch statement
+
+let day = 30;
+
+switch (day) 
+{
+    case 1:
+        document.write("It's Monday!");
+        break;
+    case 2:
+        document.write("It's Tuesday!");
+        break;
+    case 3:
+        document.write("It's Wednesday!");
+        break;
+    case 4:
+        document.write("It's Thursday!");
+        break;
+    case 5:
+        document.write("It's Friday!");
+        break;
+    case 6:
+        document.write("It's Saturday!");
+        break;
+    case 7:
+        document.write("It's Sunday!");
+        break;
+    default:
+        document.write("Invalid day of the week!");
+        break;
+}
+
